2a28ec9251142c003bb8caa03128e594740d0658
nidhiyadav-cs <nidhiyadav10y@gmail.com>
2026-09-08 23:33:16 +0530

    Added area of circle

---
+++ Circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Circle
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the radius:");
+        int radius = sc.nextInt();
+        float area = 3.14f * radius * radius;
+        System.out.println("Area of the circle = " + area);
+
+    }
+}
