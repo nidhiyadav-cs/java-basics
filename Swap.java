dbf4e80551cff562e1349f6a8540c3d37cf577ca
nidhiyadav-cs <nidhiyadav10y@gmail.com>
2026-09-12 19:54:37 +0530

    Add Swapping of 2 numbers

---
+++ Swap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Swap
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping: a is" +a);
+        System.out.println("After swapping: b is" +b);
+    }
+}
