763e77e6a238e691e2ca97a927bcf6a64a99f5fd
nidhiyadav-cs <nidhiyadav10y@gmail.com>
2026-09-24 22:30:43 +0530

    Maximum of two numbers

---
+++ Max.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Max {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        int max = Math.max(num1, num2);
+        System.out.println("The maximum number is: " + max);
+    }
+}
